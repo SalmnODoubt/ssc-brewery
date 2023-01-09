f79b619670ac404998a508912e87054ae732b75d
Johannes Biederstädt <johannes.biederstaedt@bison-group.com>
2023-01-09 19:43:58 +0100

    test

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -34,24 +35,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  PasswordEncoder passwordEncoder(){
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("spring")
-                .password("{SSHA}LT+zqH2PO+ALzl36ULU96DR/uJUSkrSi+7646Q==")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("$2a$10$A0hOiunKDCSX7AQe5Rh8uOT8Z.x0swLGhtS6a0vOrXOQQx1sKyXkG")
-                .roles("USER");
+//  @Bean
+//  PasswordEncoder passwordEncoder(){
+//    return new BCryptPasswordEncoder();
+//  }
 
-        auth.inMemoryAuthentication().withUser("scott").password("tiger").roles("CUSTOMER");
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+            .withUser("spring")
+            .password("{bcrypt}{SSHA}LT+zqH2PO+ALzl36ULU96DR/uJUSkrSi+7646Q==")
+            .roles("ADMIN")
+            .and()
+            .withUser("user")
+            .password("{bcrypt}$2a$10$A0hOiunKDCSX7AQe5Rh8uOT8Z.x0swLGhtS6a0vOrXOQQx1sKyXkG")
+            .roles("USER");
+
+    auth.inMemoryAuthentication().withUser("scott").password("tiger").roles("CUSTOMER");
+  }
+
+//  @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("spring")
+//                .password("{SSHA}LT+zqH2PO+ALzl36ULU96DR/uJUSkrSi+7646Q==")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("user")
+//                .password("$2a$10$A0hOiunKDCSX7AQe5Rh8uOT8Z.x0swLGhtS6a0vOrXOQQx1sKyXkG")
+//                .roles("USER");
+//
+//        auth.inMemoryAuthentication().withUser("scott").password("tiger").roles("CUSTOMER");
+//    }
 
     //    @Override
 //    @Bean
