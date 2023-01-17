32aa6ba5488819f263533c46600e42c06b13181a
Johannes Biederstädt <johannes.biederstaedt@bison-group.com>
2023-01-17 01:12:58 +0100

    assignment post

--- src/main/java/guru/sfg/brewery/security/perms/BeerOrderPickupPermission.java
+++ src/main/java/guru/sfg/brewery/security/perms/BeerOrderPickupPermission.java
@@ -8,8 +8,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
  * Created by jt on 7/7/20.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('order.update') OR " +
-        "hasAuthority('customer.order.update') " +
+@PreAuthorize("hasAuthority('order.pickup') OR " +
+        "hasAuthority('customer.order.pickup') " +
         " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId)" +
         " AND @beerOrderAuthenticationManger.customerIdMatchesOrderId(#customerId, #orderId)")
 public @interface BeerOrderPickupPermission {
